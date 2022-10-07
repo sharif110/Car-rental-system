6502400bc00039328751f99269aea7b1179905e9
SHARIF ALI <73417781+sharif110@users.noreply.github.com>
2022-10-08 00:19:23 +0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,103 +1,14 @@
-
-<header>
-  <div class="default-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-3 col-md-1">
-          <div class="logo"> <a href="index.php"><img src="assets/images/L1LOGO.png" width="150" 
-     height="50"alt="image"/></a> </div>
-        </div>
-        <div class="col-sm-9 col-md-10">
-          <div class="header_info">
-            <div class="header_widgets">
-            <div class="social-follow">
-              <ul>
-                <li><a href="https://www.facebook.com/sharifali.bangash.79/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-                <li><a href="https://www.facebook.com/sharifali.bangash.79/"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-                <li><a href="https://www.facebook.com/sharifali.bangash.79/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-                <li><a href="https://www.facebook.com/sharifali.bangash.79/"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
-                <li><a href="https://www.facebook.com/sharifali.bangash.79/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-              </ul>
-            </div>
-   <?php   if(strlen($_SESSION['login'])==0)
-	{
-?>
- <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
-<?php }
-else{
-
-echo "We deliver Best Services";
- } ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Navigation -->
-  <nav id="navigation_bar" class="navbar navbar-default">
-    <div class="container">
-      <div class="navbar-header">
-        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      </div>
-      <div class="header_wrap">
-        <div class="user_login">
-          <ul>
-            <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i>
-<?php
-$email=$_SESSION['login'];
-$sql ="SELECT FullName FROM CUSTOMERS WHERE EmailId=:email ";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-	{
-
-	 echo htmlentities($result->FullName); }}?><i class="fa fa-angle-down" aria-hidden="true"></i></a>
-              <ul class="dropdown-menu">
-           <?php if($_SESSION['login']){?>
-            <li><a href="profile.php">Profile Settings</a></li>
-              <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="my-booking.php">My Booking</a></li>
-            <li><a href="post-testimonial.php">Post a Testimonial</a></li>
-          <li><a href="my-testimonials.php">My Testimonial</a></li>
-            <li><a href="logout.php">Sign Out</a></li>
-            <?php } else { ?>
-            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Profile Settings</a></li>
-              <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Update Password</a></li>
-            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Booking</a></li>
-            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Post a Testimonial</a></li>
-          <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Testimonial</a></li>
-            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Sign Out</a></li>
-            <?php } ?>
-          </ul>
-            </li>
-          </ul>
-        </div>
-        <div class="header_search">
-          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
-          <form action="#" method="get" id="header-search-form">
-            <input type="text" placeholder="Click Here to find." class="form-control">
-            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-          </form>
-        </div>
-      </div>
-      <div class="collapse navbar-collapse" id="navigation">
-        <ul class="nav navbar-nav">
-          <li><a href="index.php">Home</a>    </li>
-
-          <li><a href="page.php?type=aboutus">About Us</a></li>
-   
-          <li><a href="page.php?type=faqs">FAQs</a></li>
-          <li><a href="contact-us.php">Contact Us</a></li>
-
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- Navigation end -->
-
-</header>
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 20px;">SHARIF Car Rental Portal</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ICON.jpeg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Change Password</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
